bd79730daf3f9c6d71b9ac22102fd3ec5c004394
derigel <derigel@mail.ru>
2013-06-04 22:35:24 +0400

    Sync GA info posting

--- Website/Filters/GoogleAnalyticsMeasurementProtocol.cs
+++ Website/Filters/GoogleAnalyticsMeasurementProtocol.cs
@@ -32,27 +32,35 @@ namespace NuGetGallery.Filters
         { "cid", filterContext.HttpContext.Request.Headers["X-NuGet-ApiKey"] ?? filterContext.HttpContext.Request.Headers["NuGet-ApiKey"]  ?? Guid.NewGuid().ToString() },
         { "ni",  "1" },
       };
-      
-      var viewParameters = new NameValueCollection(baseParameters)
-      {
-        { "t",  "pageview" },
-        { "dl", filterContext.HttpContext.Request.RawUrl },
-      };
-      webClient.UploadValuesAsync(GA_ENDPOINT, viewParameters);
 
-      var packageId = string.Join(",", 
-        new [] { filterContext.RouteData.Values["id"], filterContext.RouteData.Values["version"] }
-          .Select(_ => _ != null)
-          .Select(_ => Convert.ToString(_)));
-
-      var eventParameters = new NameValueCollection(baseParameters)
+      try
       {
-        { "t",  "event" },
-        { "ec", "NuGet" },
-        { "ea", filterContext.HttpContext.Request.Headers["NuGet-Operation"] ?? "Unknown" },
-        { "ev", packageId },
-      };
-      webClient.UploadValuesAsync(GA_ENDPOINT, eventParameters);
+        var viewParameters = new NameValueCollection(baseParameters)
+        {
+          { "t",  "pageview" },
+          { "dl", filterContext.HttpContext.Request.RawUrl },
+        };
+        webClient.UploadValues(GA_ENDPOINT, viewParameters);
+      }
+      catch { }
+
+      try
+      {
+        var packageId = string.Join(",", 
+          new [] { filterContext.RouteData.Values["id"], filterContext.RouteData.Values["version"] }
+            .Select(_ => _ != null)
+            .Select(_ => Convert.ToString(_)));
+
+        var eventParameters = new NameValueCollection(baseParameters)
+        {
+          { "t",  "event" },
+          { "ec", "NuGet" },
+          { "ea", filterContext.HttpContext.Request.Headers["NuGet-Operation"] ?? "Unknown" },
+          { "ev", packageId },
+        };
+        webClient.UploadValuesAsync(GA_ENDPOINT, eventParameters);
+      }
+      catch { }
     }
   }
 }
