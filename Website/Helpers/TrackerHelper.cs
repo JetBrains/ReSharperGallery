6f1fbb30ab97db8c1b8a290674b29911abdfe697
derigel <derigel@mail.ru>
2013-07-23 20:11:13 +0400

    Update GoogleAnalyticsTracker package to 1.6.005

--- Website/Helpers/TrackerHelper.cs
+++ Website/Helpers/TrackerHelper.cs
@@ -1,20 +1,11 @@
 ﻿using System.Net;
 using System.Net.Http;
-using System.Web;
 using GoogleAnalyticsTracker;
 
 namespace NuGetGallery.Helpers
 {
   public static class TrackerHelper
   {
-    public static void TrackPageView(this Tracker tracker, HttpRequestBase request, string pageTitle, string pageUrl)
-    {
-      tracker.Hostname = request.Url != null ? request.Url.Host : Dns.GetHostName();
-      tracker.UserAgent = request.UserAgent;
-      tracker.Language = request.UserLanguages != null ? string.Join(";", request.UserLanguages) : "";
-      tracker.TrackPageView(pageTitle, pageUrl);
-    }
-
     public static void TrackPageView(this Tracker tracker, HttpRequestMessage request, string pageTitle, string pageUrl)
     {
       tracker.Hostname = request.RequestUri != null ? request.RequestUri.Host : Dns.GetHostName();
